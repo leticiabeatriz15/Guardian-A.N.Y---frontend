544578e56a49dfe49217483d70751df7c492beff
Leticia Beatriz <leticiabeatriz1508@gmail.com>
2026-07-25 00:30:25 -0300

    Conexao com o back

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/analisar";
+const API_URL = "https://guardian-a-n-y-backend.onrender.com/analisar";;
 
 // Seletores do DOM
 const videoGrid = document.getElementById("videoGrid");
